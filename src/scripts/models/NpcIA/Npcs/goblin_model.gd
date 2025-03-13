2741d3c4180845ff2ee5936604e8ecb440166e5e
juan pablo maletti <juanpi.maletti@gmail.com>
2025-03-13 12:17:36 -0300

    Update goblin_model.gd

--- src/scripts/models/NpcIA/Npcs/goblin_model.gd
+++ src/scripts/models/NpcIA/Npcs/goblin_model.gd
@@ -6,14 +6,20 @@ func state_idle(npc, target):
     if is_my_turn(npc):
         var npc_threshold = get_health_threshold(npc)
         var target_threshold = get_health_threshold(target)
-        
+        var prob = get_prob()
         # Si el NPC está en HIGH y el objetivo en VERY_LOW => ARROGANCE
         if npc_threshold == Enums.HealthThreshold.HIGH and target_threshold == Enums.HealthThreshold.VERY_LOW:
-            current_state = Enums.NPCStates.ARROGANCE
+            if prob < 0.15:
+                current_state = Enums.NPCStates.ARROGANCE
+            else:
+                current_state = Enums.NPCStates.DECISION
         
         # Si el NPC está por debajo de 25% => BERSERK
         elif npc_threshold == Enums.HealthThreshold.VERY_LOW:
-            current_state = Enums.NPCStates.BERSERK
+            if prob < 0.15:
+                current_state = Enums.NPCStates.BERSERK
+            else:
+                current_state = Enums.NPCStates.DECISION
         
         # Caso contrario => DECISION
         else:
